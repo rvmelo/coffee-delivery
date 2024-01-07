e001d0c427ed00784e0fa0b8d14f4fbf0d5eb54d
rvmelo <robertomelo7@hotmail.com>
2024-01-07 12:06:55 -0300

    first commit

---
+++ src/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+:focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme['purple-dark']};
+  }
+
+  body {
+    background: ${({ theme }) => theme.background};
+    color: ${({ theme }) => theme['base-text']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+`
