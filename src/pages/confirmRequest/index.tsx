070d0d1c8172c20f2f45e66cbb9a65cc82ed0685
rvmelo <robertomelo7@hotmail.com>
2024-02-06 09:25:29 -0300

    feat: add confirmation page

--- src/pages/confirmRequest/index.tsx
+++ src/pages/confirmRequest/index.tsx
@@ -1,11 +1,63 @@
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
 import React from 'react'
-
-// import { Container } from './styles';
+import {
+  AddressContainer,
+  AddressItem,
+  ConfirmRequestContainer,
+  IconWrapper,
+  IllustrationGrid,
+  ItemTextWrapper,
+  RequestGrid,
+  RequestStatusContainer,
+} from './styles'
+import { useTheme } from 'styled-components'
+import confirmation from '../../assets/confirmation.svg'
 
 export const ConfirmRequest: React.FC = () => {
+  const theme = useTheme()
+
   return (
-    <div>
-      <span>confirm request</span>
-    </div>
+    <ConfirmRequestContainer>
+      <RequestGrid>
+        <RequestStatusContainer>
+          <span className="title">Uhu! Pedido confirmado</span>
+          <span>Agora é só aguardar que logo o café chegará até você</span>
+        </RequestStatusContainer>
+        <AddressContainer>
+          <AddressItem>
+            <IconWrapper backgroundColor={theme.purple}>
+              <MapPin />
+            </IconWrapper>
+            <ItemTextWrapper>
+              <span>
+                Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+              </span>
+              <span>Farrapos - Porto Alegre, RS</span>
+            </ItemTextWrapper>
+          </AddressItem>
+          <AddressItem>
+            <IconWrapper backgroundColor={theme['yellow-dark']}>
+              <Timer />
+            </IconWrapper>
+            <ItemTextWrapper>
+              <span>Previsão de entrega </span>
+              <strong>20 min - 30 min</strong>
+            </ItemTextWrapper>
+          </AddressItem>
+          <AddressItem>
+            <IconWrapper backgroundColor={theme['yellow-dark']}>
+              <CurrencyDollar />
+            </IconWrapper>
+            <ItemTextWrapper>
+              <span>Pagamento na entrega</span>
+              <strong>Cartão de Crédito</strong>
+            </ItemTextWrapper>
+          </AddressItem>
+        </AddressContainer>
+      </RequestGrid>
+      <IllustrationGrid>
+        <img src={confirmation} alt="" />
+      </IllustrationGrid>
+    </ConfirmRequestContainer>
   )
 }
---
+++ src/pages/confirmRequest/styles.ts
@@ -0,0 +1,100 @@
+import styled from 'styled-components'
+
+interface IconWrapperProps {
+  backgroundColor: string
+}
+
+export const ConfirmRequestContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 6.375rem;
+  padding: 0 10rem;
+  margin-top: 5rem;
+`
+
+export const RequestStatusContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+
+  .title {
+    font-family: 'Baloo 2';
+    font-size: 2rem;
+    color: ${({ theme }) => theme.yellow};
+  }
+
+  span {
+    font-size: 1.25rem;
+    color: ${({ theme }) => theme['base-subtitle']};
+  }
+`
+
+export const AddressContainer = styled.div`
+  border-top-right-radius: 36px;
+  border-bottom-left-radius: 36px;
+
+  border-top-left-radius: 6px;
+  border-bottom-right-radius: 6px;
+
+  margin-top: 2.5rem;
+
+  border-color: transparent;
+  background-origin: border-box;
+  background-image: ${({ theme }) =>
+    `linear-gradient(to bottom right, ${theme['yellow-dark']}, ${theme.purple})`};
+
+  padding: 40px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`
+
+export const AddressItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 12px;
+`
+
+export const IconWrapper = styled.div<IconWrapperProps>`
+  width: 2rem;
+  height: 2rem;
+  border-radius: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background: ${({ backgroundColor }) => backgroundColor};
+
+  svg {
+    color: ${({ theme }) => theme.background};
+  }
+`
+
+export const ItemTextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+
+  span {
+    font-size: 1rem;
+    color: ${({ theme }) => theme['base-text']};
+  }
+
+  strong {
+    font-size: 1rem;
+    color: ${({ theme }) => theme['base-text']};
+    font-weight: 700;
+  }
+`
+
+export const RequestGrid = styled.div``
+
+export const IllustrationGrid = styled.div`
+  img {
+    width: 30.75rem;
+    height: 18.3rem;
+  }
+`
